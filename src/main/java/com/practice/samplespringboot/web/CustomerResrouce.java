e9bcfe023c2a90e7c64675ef15ef77d832a3d586
Naveen_Mittal <naveen.mittal@kloudspot.com>
2026-09-19 14:18:54 +0530

    removed unused resrouces

--- src/main/java/com/practice/samplespringboot/web/CustomerResrouce.java
+++
@@ -1,8 +0,0 @@
-package com.practice.samplespringboot.web;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CustomerResrouce {
-
-}
--- src/main/java/com/practice/samplespringboot/web/TransactionResrouce.java
+++
@@ -1,4 +0,0 @@
-package com.practice.samplespringboot.web;
-
-public class TransactionResrouce {
-}
